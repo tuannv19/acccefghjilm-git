eb9e499c05fe4c1295e52b6880c77c0900920751
tuan.nguyenv <tuan.nguyenv@sc3.com.vn>
2017-12-28 22:29:04 +0700

    First commit

---
+++ demoApp/demoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoApp
+//
+//  Created by tuannv on 12/28/17.
+//  Copyright © 2017 tuannv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoApp/demoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoApp
+//
+//  Created by tuannv on 12/28/17.
+//  Copyright © 2017 tuannv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
